f74a4372bb2832d1e689151fdfc612dd03d5bb6d
Egor Fortov <egorfortov@gmail.com>
2024-11-03 18:42:45 +0300

    added film & user services

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {  // добавление в друзья, удаление из друзей, вывод списка общих друзей.
+
+}
