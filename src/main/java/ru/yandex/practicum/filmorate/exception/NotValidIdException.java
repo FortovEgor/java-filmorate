58aa670b96a7d3c95bc61b478fd4e8bfda9a60cc
Egor Fortov <egorfortov@gmail.com>
2024-11-03 21:05:10 +0300

    tiny fixes

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotValidIdException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotValidIdException.java
@@ -2,6 +2,6 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class NotValidIdException extends RuntimeException {
     public NotValidIdException() {
-        super("id должен быть больше ноля.");
+        super("id должен быть > ноля.");
     }
 }
