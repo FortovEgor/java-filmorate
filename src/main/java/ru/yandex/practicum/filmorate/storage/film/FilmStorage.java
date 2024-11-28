c6f182004f30e91aaf2fe33d5f20e1ee0da6246d
Egor Fortov <egorfortov@gmail.com>
2024-11-28 12:24:10 +0300

    PR fixes

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -4,7 +4,6 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
-import java.util.Map;
 
 public interface FilmStorage {
     // методы добавления, удаления и модификации объектов
